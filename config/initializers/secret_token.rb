15ea3b5171e75adb48ae5a1b9f861d2a6f4a7a49
Weroth <weroth@gmail.com>
2014-01-23 14:32:16 +0800

    Initial commit.Setup refinery news plugin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtsRefine::Application.config.secret_token = '8f7bd3afa946797b101186ee6e572cf259ccba20369e96c1f05f757e0901acd9c80bf023194f93a0e70fe3de38f5ffcb7fa25acdc4884cd576d0df1efc8c43d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ArtsRefine::Application.config.session_store :cookie_store, key: '_arts_refine_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ArtsRefine::Application.config.session_store :active_record_store
